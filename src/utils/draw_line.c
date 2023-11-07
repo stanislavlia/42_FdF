6d6ca53d2598883b9865e0c94191b5116515b8b0
Stanislav Liashkov <sliashko@z2t3c1.42bangkok.com>
2023-11-07 19:16:19 +0700

    done with Bresenham's alg for drawing a line

--- src/utils/draw_line.c
+++ src/utils/draw_line.c
@@ -1,44 +1,108 @@
 
 #include "fdf.h"
 
+
+int	is_steep(t_vector p1, t_vector p2)
+{
+	if (abs(p2.y - p1.y) > abs(p2.x - p1.x))
+		return (1);
+	return (0);
+}
+
 //Implementation of Bresenham's algorithm
 
-//For now, 
-//		   I assume x2 > x1
-//				and y2 > y1
-// 			For now func doesnt work with great slope + some edge cases
-void	draw_line(t_vector	point1, t_vector point2, int color, t_env env)
+//Plots line with small steep (x changes faster than y)
+// Reduce num of vars
+void	draw_linelow(t_vector p1, t_vector p2)
 {
-	int	deltaY;
-	int	deltaX;
+	int	dx;
+	int	dy;
+	int	y_step;
 	int	x;
-	int	y;
-	int	D;
-
-	
-	deltaY = point2.y - point1.y;
-	deltaX = point2.x - point1.x;
-	x = point1.x;
-	y = point1.y;
-	D = 2*deltaY - deltaX;
-
-	printf("point1: x = %d y = %d\n", point1.x, point1.y);
-	printf("point2: x = %d y = %d\n", point2.x, point2.y);
-	printf("Dx = %d; Dy = %d\n", deltaX, deltaY);
+	int y;
+	int D;
 
 
-	while (x < point2.x)
+	x = p1.x;
+	y = p1.y;
+	y_step = 1;
+	dx = p2.x - p1.x;
+	dy = p2.y - p1.y;
+	if (dy < 0)
 	{
-		printf("plot x=%d; y=%d\n", x, y);
+		y_step = -1;
+		dy = -dy;
+	}
+	D = (2 * dy) - dx;
+	while (x < p2.x)
+	{
+		printf("plot x = %d; y = %d\n", x, y);
 		if (D > 0)
 		{
-			y++;
-			D = D - 2 * deltaX;
+			y = y + y_step;
+			D = D + (2 * (dy - dx));
 		}
-		D = D + 2 * deltaY;
+		else
+			D = D + 2 * dy;
 		x++;
 	}
 }
+
+//Basically swaps x and y axises
+// replace x with y in func for low steep
+void	draw_linehigh(t_vector p1, t_vector p2)
+{
+	int	dx;
+	int	dy;
+	int	x_step;
+	int	x;
+	int y;
+	int D;
+
+
+	x = p1.x;
+	y = p1.y;
+	x_step = 1;
+	dx = p2.x - p1.x;
+	dy = p2.y - p1.y;
+	if (dx < 0)
+	{
+		x_step = -1;
+		dx = -dx;
+	}
+	D = (2 * dx) - dy;
+	while (y < p2.y)
+	{
+		printf("plot x = %d; y = %d\n", x, y);
+		if (D > 0)
+		{
+			x = x + x_step;
+			D = D + (2 * (dx - dy));
+		}
+		else
+			D = D + 2 * dx;
+		y++;
+	}
+}
+
+void	draw_line(t_vector p1, t_vector p2, t_env *env)
+{
+	if	(is_steep(p1, p2))
+	{
+		if (p2.y > p1.y)
+			draw_linehigh(p1, p2);
+		else
+			draw_linehigh(p2, p1);
+	}
+	else
+	{
+		if (p2.x > p1.x)
+			draw_linelow(p1, p2);
+		else
+			draw_linelow(p2, p1);
+	}
+}
+
 
 int	main()
 {
@@ -46,9 +110,7 @@ int	main()
 	t_vector	p2;
 	t_env		env;
 
-
-	p1.x = 0; p1.y = 1;
-	p2.x = 2; p2.y = 5;
-	draw_line(p1, p2, 100, env);
-	
+	p1.x = 0; p1.y = -4;
+	p2.x = 0; p2.y = -10;
+	draw_line(p1, p2, &env);
 }
